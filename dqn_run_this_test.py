c695cb609e74a4ead2a763e7514ce4daba854cae
zm2229 <zm2229@163.com>
2019-07-19 00:56:36 +0100

    update DQN_modified_pong_t2.py dqn_run_this_test.py

--- dqn_run_this_test.py
+++ dqn_run_this_test.py
@@ -1,9 +1,11 @@
 from connect4_selfplay import Connect4
-from DQN_modified_test import DeepQNetwork
+from DQN_modified_pong_t2 import DeepQNetwork
+#from DQN_modified_test import DeepQNetwork
 import cv2
 import numpy as np
 import os
 import csv
+import datetime
 
 
 def run_connect4():
@@ -29,7 +31,8 @@ def run_connect4():
     writer = csv.writer(csv_file)
 
     i = 0
-    for episode in range(30):
+    #for episode in range(40000):
+    for episode in range(50000):
     # every game number
     #while True:
         game_number += 1
@@ -42,6 +45,9 @@ def run_connect4():
         reward_p2_total += reward_p1[1]
         info_p1_total = np.sum([info_p1_total, info_p1[:4]], axis=0)
         info_p2_total = np.sum([info_p2_total, info_p1[-4:]], axis=0)
+        observation_p1 = RL.pre_process(observation_p1, crop_size)
+       # observation_p1 = np.reshape(observation_p1, (60, 60, 1))
+        observation_p1 = np.stack((observation_p1, observation_p1, observation_p1, observation_p1), axis=2)
        #  print("【action_p1: %d,  done_p1: %s】:\nreward_p1: %d, reward_p1_total: %d, info_p1[n_w, n_d, n_l, n_il]: %s" %(info_p1[4], str(done_p1), reward_p1[0], reward_p1_total , str(info_p1[:4])))
        #  print("reward_p2: %d, reward_p2_total: %d, info_p2[n_w, n_d, n_l, n_il]: %s" %(reward_p1[1], reward_p2_total, str(info_p1[-4:])))
        # # print("chessboard:\n", info_p1[5])
@@ -52,7 +58,7 @@ def run_connect4():
         while True:
             # fresh env
             # RL choose action based on observation
-            observation_p1 = RL.pre_process(observation_p1, crop_size)
+
         #    cv2.imwrite(image_path + "%d_p1_pre.png" % i, observation_p1)
             action_p2 = RL.choose_action(observation_p1)
             # RL take action and get next observation and reward
@@ -63,6 +69,11 @@ def run_connect4():
             reward_p1_total += reward_p2[0]
             info_p2_total = np.sum([info_p2_total, info_p2[-4:]], axis=0)
             info_p1_total = np.sum([info_p1_total, info_p2[:4]], axis=0)
+
+            observation_p2 = RL.pre_process(observation_p2, crop_size)
+            observation_p2 = np.reshape(observation_p2, (60, 60, 1))
+            observation_p2 = np.append(observation_p2, observation_p1[:, :, 0:3], axis=2)
+            RL.store_transition(observation_p1, action_p2, reward_p2[1], observation_p2)
          #    print("【action_p2: %d,  done_p2: %s】:\nreward_p2: %d, reward_p2_total: %d, info_p2[n_w, n_d, n_l, n_il]: %s" %(info_p2[4], str(done_p2), reward_p2[1], reward_p2_total , str(info_p2[-4:])))
          #    print("reward_p1: %d, reward_p1_total: %d, info_p1[n_w, n_d, n_l, n_il]: %s" %(reward_p2[0], reward_p1_total, str(info_p2[:4])))
          #
@@ -73,11 +84,6 @@ def run_connect4():
             if done_p2:
                 break
 
-            observation_p2 = RL.pre_process(observation_p2, crop_size)
-         #   cv2.imwrite(image_path + "%d_p2_pre.png" % i, observation_p2)
-            observation_p1 = observation_p1.flatten()
-            observation_p2 = observation_p2.flatten()
-            RL.store_transition(observation_p1, action_p2, reward_p2[1], observation_p2)
 
             if (step_p2 > 200) and (step_p2 % 1 == 0):
                 RL.learn(step_p2)
@@ -88,6 +94,12 @@ def run_connect4():
             reward_p2_total += reward_p1[1]
             info_p1_total = np.sum([info_p1_total, info_p1[:4]], axis=0)
             info_p2_total = np.sum([info_p2_total, info_p1[-4:]], axis=0)
+
+            observation_p1 = RL.pre_process(observation_p1, crop_size)
+            observation_p1 = np.reshape(observation_p1, (60, 60, 1))
+            observation_p1 = np.append(observation_p1, observation_p2[:, :, 0:3], axis=2)
+            # RL.store_transition(observation_p2, action, reward, observation_p1)
+
           #   print("【action_p1: %d,  done_p1: %s】:\nreward_p1: %d, reward_p1_total: %d, info_p1[n_w, n_d, n_l, n_il]: %s" % (info_p1[4], str(done_p1), reward_p1[0], reward_p1_total , str(info_p1[:4])))
           #   print("reward_p2: %d, reward_p2_total: %d, info_p2[n_w, n_d, n_l, n_il]: %s" %(reward_p1[1], reward_p2_total, str(info_p1[-4:])))
           # #  print("action_p1: %d, reward_p1: %d, reward_p1_total: %d, done_p1: %s, info_p1[n_w, n_d, n_l, n_il]: %s" %(info_p1[4], reward_p1, reward_p1_total, str(done_p1), str(info_p1[:4])))
@@ -120,10 +132,11 @@ def run_connect4():
         print("reward_p2_total: %d, info_p2_total[n_w, n_d, n_l, n_il]: %s, step_p2: %d." % (reward_p2_total, str(info_p2_total), step_p2))
         print("*" * 80, "\n")
 
-        csv_data = [win_rate_p1, win_rate_p2, avg_step_p1, avg_step_p2, reward_p1_total, reward_p2_total]
-        if game_number % 3 == 0:
+        csv_data = [win_rate_p1, win_rate_p2, avg_step_p1, avg_step_p2, reward_p1_total, reward_p2_total, info_p1_total[0], info_p1_total[1], info_p1_total[2], info_p1_total[3], info_p2_total[0], info_p2_total[1], info_p2_total[2], info_p2_total[3]]
+        if game_number % 100 == 0:
             writer.writerow(csv_data)
-        if game_number % 3 == 0:
+            csv_file.flush()
+        if game_number % 1000 == 0:
             RL.plt_data(project_path + "\images\\", win_rate_p1_list, win_rate_p2_list, avg_step_p1_list, avg_step_p2_list, reward_p1_list, reward_p2_list)
 
 
@@ -141,19 +154,35 @@ def run_connect4():
 
 if __name__ == "__main__":
     # maze game
+    start_time = datetime.datetime.now()
+    print("start time:" + start_time.strftime('%Y-%m-%d %H:%M:%S'))
     env = Connect4()
     mode_type = 'train'
     #mode_type = 'use mode'
-    RL = DeepQNetwork(mode=mode_type, n_actions=7, n_features=60*60*1,
-                      learning_rate=0.01,
-                      reward_decay=0.9,
-                      e_greedy=0.9,
-                      replace_target_iter=200,
-                      memory_size=2000,
-                      output_graph=True
+    # RL = DeepQNetwork(mode=mode_type, n_actions=7, n_features=60*60*1,
+    #                   learning_rate=0.01,
+    #                   reward_decay=0.9,
+    #                   e_greedy=0.9,
+    #                   replace_target_iter=200,
+    #                   memory_size=2000,
+    #                   output_graph=True
+    #                   )
+    RL = DeepQNetwork(n_actions=7,
+                      output_graph=False
                       )
     if not os.path.exists('./checkpoints'):
         os.makedirs('./checkpoints')
+    if not os.path.exists('./csv_data'):
+        os.makedirs('./csv_data')
+    if os.path.exists('./csv_data/result_data.csv'):
+        os.system('mv ./csv_data/result_data.csv ./csv_data/result_data1.csv')
+
+
+    if not os.path.exists('./images'):
+        os.makedirs('./images')
     run_connect4()
 
     RL.plot_cost()
+    end_time = datetime.datetime.now()
+    print("end time:" + end_time.strftime('%Y-%m-%d %H:%M:%S'))
+    print("run time(seconds):" + (end_time - start_time).seconds)
